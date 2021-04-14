bc55a82bbdf43c06687622da7c7c923ffae173af
Chris Tam <ohgodtamit@gmail.com>
2021-04-14 12:32:58 -0400

    Revise 2017-1 solution

--- 2017/1/solution.rs
+++ 2017/1/solution.rs
@@ -1,48 +1,52 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::{
+    convert::TryInto,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn part1(line: &str) -> i32 {
     // python code:
-    // 
+    //
     // s = sum(int(a) for a, b in zip(line, line[1:] + [line[0]]) if a == b)
 
-    let other = &line[1..];
-    let mut s = 0;
-    for (a, b) in line.chars().zip(other.chars()) {
-        if a == b {
-            s += a.to_digit(10).unwrap() as i32;
-        }
-    }
-    let first = line.chars().next().unwrap();
-    let last = line.chars().rev().next().unwrap();
-    if first == last {
-        s += first.to_digit(10).unwrap() as i32;
-    }
-    return s;
+    line.chars()
+        .zip(line.chars().cycle().skip(1))
+        .fold(0, |acc, (a, b)| {
+            if a == b {
+                acc + a.to_digit(10).expect("Could not convert char to digit")
+            } else {
+                acc
+            }
+        })
+        .try_into()
+        .expect("Failed to convert to i32")
 }
 
 fn part2(line: &str) -> i32 {
-    let len = line.len();
-    let other = &line[len / 2..];
-    let mut s = 0;
-    for (a, b) in line.chars().zip(other.chars()) {
-        if a == b {
-            s += a.to_digit(10).unwrap() as i32;
-        }
-    }
-    return s * 2;
+    let sum: i32 = line
+        .chars()
+        .zip(line.chars().skip(line.len() / 2))
+        .fold(0, |acc, (a, b)| {
+            if a == b {
+                acc + a.to_digit(10).expect("Could not convert char to digit")
+            } else {
+                acc
+            }
+        })
+        .try_into()
+        .expect("Failed to convert to i32");
+
+    sum * 2
 }
 
 fn main() {
     let filename = "input";
     let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
 
-    let mut vec = Vec::new();
-    for line in reader.lines() {
-        let line = line.unwrap();
-        vec.push(line);
-    }
+    let vec = BufReader::new(file)
+        .lines()
+        .map(|line| line.expect("Could not read line from file"))
+        .collect::<Vec<String>>();
 
     let line = &vec[0];
     println!("{}", part1(line));
